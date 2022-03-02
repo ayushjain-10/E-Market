8e33cd3b6633682776d85f000fdb96f9bffc9aec
Ayush Jain <ayushshreeshreemal@gmail.com>
2022-03-02 00:26:18 -0800

    updated config file

--- market/__init__.py
+++ market/__init__.py
@@ -2,11 +2,16 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+import os
+from market.config import Config
+
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///market.db"
-app.config['SECRET_KEY'] = "1275e186f427443a6ceb5c75"
+# app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///market.db"
+# app.config['SECRET_KEY'] = "1275e186f427443a6ceb5c75"
+app.config.from_object(Config)
+app.secret_key = os.urandom(24)
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
