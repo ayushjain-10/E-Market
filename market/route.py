a7b45eeeaeaa61c3aac1a2150f02d589b5393c57
Ayush Jain <ayushshreeshreemal@gmail.com>
2022-03-03 21:18:24 -0800

    update

--- market/route.py
+++ market/route.py
@@ -5,6 +5,9 @@ from market.forms import RegisterForm, LoginForm, PurchaseItemForm, SellItemForm
 from market import db
 from flask_login import login_user, logout_user, login_required, current_user
 
+with app.app_context():
+    db.create_all()
+
 @app.route('/')
 @app.route('/home')
 def home_page():
