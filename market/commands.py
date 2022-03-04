d93e85aeb0c7b07a168e334c9fe02b0dd99c12e5
Ayush Jain <ayushshreeshreemal@gmail.com>
2022-03-03 22:46:01 -0800

    update

--- market/commands.py
+++ market/commands.py
@@ -1,9 +1,6 @@
 import click
 from flask.cli import with_appcontext
 
-from flask import render_template, redirect, url_for, flash, request
-from market.models import Items, User
-from market.forms import RegisterForm, LoginForm, PurchaseItemForm, SellItemForm
 from market import db
 from flask_login import login_user, logout_user, login_required, current_user
 
